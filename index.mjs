285474d888a7d27e803d5b91a752557490941491
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-07-16 11:19:21 -0400

    Added mongodb connection via new conn.mjs file

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+//
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the API.");
+});
+
+// Global error handling
+app.use((err, _req, res, next) => {
+    res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
